70ab83cf8cf8e0ea3665f41f8379dac1a5a827e8
Maxim Shchenkov <goodok61@gmail.com>
2020-02-25 23:22:33 +0300

    Урок 15 правки

--- script/script.js
+++ script/script.js
@@ -4,16 +4,9 @@
 2) Переделать наш проект под класс с помощью ключевого слова Class и Constuctor()
 3) Переменные, существующие только с неизменяемым параметром, объявить через const.
 */
-let isNumber = function (n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-};
+const isNumber = n => !isNaN(parseFloat(n)) && isFinite(n);
 
-function isEmpty(str) {
-  if (str.trim() == "") {
-    return true;
-  }
-  return false;
-}
+// const isEmpty = str => (str.trim() === "");
 
 const start = document.getElementById("start"),
   reset = document.getElementById('cancel'),
@@ -29,11 +22,9 @@ const start = document.getElementById("start"),
   incomePeriodValue = document.querySelector(".income_period-value"),
   targetMonthValue = document.querySelector(".target_month-value"),
   salaryAmount = document.querySelector(".salary-amount"),
-  incomeItems = document.querySelectorAll(".income-items"),
   incomeTitle = document.querySelector(".income-title"),
   incomeAmount = document.querySelector(".income-amount"),
   expensesTitle = document.querySelector(".expenses-title"),
-  expensesItems = document.querySelectorAll(".expenses-items"),
   additionalExpensesItem = document.querySelector(".additional_expenses-item"),
   depositBank = document.querySelector(".deposit-bank"),
   depositAmount = document.querySelector(".deposit-amount"),
@@ -42,6 +33,8 @@ const start = document.getElementById("start"),
   periodSelect = document.querySelector(".period-select"),
   periodAmount = document.querySelector(".period-amount");
 
+let incomeItems = document.querySelectorAll(".income-items"),
+  expensesItems = document.querySelectorAll(".expenses-items");
 
 
 class AppData {
@@ -58,10 +51,8 @@ class AppData {
     this.budgetMonth = 0;
     this.expensesMonth = 0;
     this.incomeMonth = 0;
-    // console.log(this);
   }
   start() {
-    // console.log(this);
     this.budget = +salaryAmount.value;
     this.getPeriod();
     this.getExpenses();
@@ -71,18 +62,14 @@ class AppData {
     this.getAddExpenses();
     this.getAddIncome();
     this.getBudget();
-    // console.log("Расходы за месяц составляют: " + appData.expensesMonth);
     this.getTargetMonth();
     this.getStatusIncome();
-    //appData.result();
     this.getInfoDeposit();
     this.consoleExpenses();
     this.showResult();
     this.endProgramm();
   }
   showResult() {
-    // console.log(this);
-    const _this = this;
     budgetMonthValue.value = this.budgetMonth;
     budgetDayValue.value = Math.floor(this.budgetDay);
     expensesMonthValue.value = this.expensesMonth;
@@ -90,12 +77,10 @@ class AppData {
     additionalIncomeValue.value = this.addIncome.join(", ");
     targetMonthValue.value = Math.ceil(this.getTargetMonth());
     incomePeriodValue.value = this.calcSavedMoney();
-    periodSelect.addEventListener("input", function () {
-      incomePeriodValue.value = _this.calcSavedMoney();
-    });
+    periodSelect.addEventListener("input", () => incomePeriodValue.value = this.calcSavedMoney());
   }
   addExpensesBlock() {
-    let cloneExpensesItem = expensesItems[0].cloneNode(true);
+    const cloneExpensesItem = expensesItems[0].cloneNode(true);
     cloneExpensesItem.querySelector(".expenses-title").value = "";
     cloneExpensesItem.querySelector(".expenses-amount").value = "";
     expensesItems[0].parentNode.insertBefore(cloneExpensesItem, plusExpenses);
@@ -105,7 +90,7 @@ class AppData {
     }
   }
   addIncomeBlock() {
-    let cloneIncomeItem = incomeItems[0].cloneNode(true);
+    const cloneIncomeItem = incomeItems[0].cloneNode(true);
     cloneIncomeItem.querySelector(".income-title").value = "";
     cloneIncomeItem.querySelector(".income-amount").value = "";
     incomeItems[0].parentNode.insertBefore(cloneIncomeItem, plusIncome);
@@ -115,43 +100,39 @@ class AppData {
     }
   }
   getExpenses() {
-    const _this = this;
-    expensesItems.forEach(function (item) {
-      let itemExpenses = item.querySelector(".expenses-title").value;
-      let cashExpenses = item.querySelector(".expenses-amount").value;
+    expensesItems.forEach(item => {
+      const itemExpenses = item.querySelector(".expenses-title").value;
+      const cashExpenses = item.querySelector(".expenses-amount").value;
       if (itemExpenses !== "" && cashExpenses !== "") {
-        _this.expenses[itemExpenses] = +cashExpenses;
+        this.expenses[itemExpenses] = +cashExpenses;
       }
     });
   }
   getIncome() {
-    const _this = this;
-    incomeItems.forEach(function (item) {
-      let itemIncome = item.querySelector(".income-title").value;
-      let cashIncome = item.querySelector(".income-amount").value;
+    incomeItems.forEach(item => {
+      const itemIncome = item.querySelector(".income-title").value;
+      const cashIncome = item.querySelector(".income-amount").value;
       if (itemIncome !== "" && cashIncome !== "") {
-        _this.income[itemIncome] = +cashIncome;
+        this.income[itemIncome] = +cashIncome;
       }
     });
   }
   getAddExpenses() {
-    const _this = this;
     this.addExpenses = [];
-    let addExpenses = additionalExpensesItem.value.split(",");
-    addExpenses.forEach(function (item) {
+    const addExpenses = additionalExpensesItem.value.split(",");
+    addExpenses.forEach(item => {
       item = item.trim();
       if (item !== "") {
-        _this.addExpenses.push(item);
+        this.addExpenses.push(item);
       }
     });
   }
   getAddIncome() {
-    const _this = this;
     this.addIncome = [];
-    additionalIncomeItem.forEach(function (item) {
-      let itemValue = item.value.trim();
+    additionalIncomeItem.forEach(item => {
+      const itemValue = item.value.trim();
       if (itemValue !== "") {
-        _this.addIncome.push(itemValue);
+        this.addIncome.push(itemValue);
       }
     });
   }
@@ -209,20 +190,14 @@ class AppData {
     const b = this.addExpenses.map(item => item[0].toUpperCase() + item.slice(1));
   }
   endProgramm() {
-    // console.log("ok");
-    let leftSide = document.querySelector(".data"),
-      leftInputs = leftSide.querySelectorAll("input[type=text]");
-    leftInputs.forEach(function (item) {
+    const leftInputs = document.querySelector(".data").querySelectorAll("input[type=text]");
+    leftInputs.forEach(item => {
       item.disabled = true;
     });
     start.style.display = 'none';
     reset.style.display = 'block';
-    // start.textContent = "Сбросить";
-    // start.removeEventListener("click", this.start);
-    // start.addEventListener("click", this.resetProgramm);
     plusExpenses.style.display = "none";
     plusIncome.style.display = "none";
-    // console.log(this);
   }
   resetProgramm() {
     this.income = {};
@@ -239,33 +214,20 @@ class AppData {
     this.incomeMonth = 0;
     periodSelect.value = 1;
     periodAmount.textContent = 1;
-    start.textContent = "Рассчитать";
-    let leftSide = document.querySelector(".data"),
-      leftInputs = leftSide.querySelectorAll("input[type=text]"),
+    const leftInputs = document.querySelector(".data").querySelectorAll("input[type=text]"),
       allTextInput = document.querySelectorAll("input[type=text]");
-    allTextInput.forEach(function (item) {
-      item.value = "";
-    });
-    leftInputs.forEach(function (item) {
-      item.disabled = false;
-    });
+    allTextInput.forEach(item => item.value = "");
+    leftInputs.forEach(item => item.disabled = false);
     checkSalaryAmount();
-    // start.removeEventListener("click", this.resetProgramm);
-    // start.addEventListener("click", this.start);
     start.style.display = "block";
     reset.style.display = "none";
     plusExpenses.style.display = "block";
     plusIncome.style.display = "block";
-    // console.log(this);
   }
-  eventlisteners() {
-    console.log(this);
-    console.log(appData);
-    console.log(this.constructor.name);
-    console.log(this.constructor);
+  eventListeners() {
     salaryAmount.addEventListener("change", checkSalaryAmount);
-    start.addEventListener("click", this.start.bind(appData));
-    reset.addEventListener("click", this.resetProgramm);
+    start.addEventListener("click", this.start.bind(this));
+    reset.addEventListener("click", this.resetProgramm.bind(this));
     plusIncome.addEventListener("click", this.addIncomeBlock);
     plusExpenses.addEventListener("click", this.addExpensesBlock);
     periodSelect.addEventListener("input", this.getPeriod);
@@ -273,14 +235,13 @@ class AppData {
 };
 
 const appData = new AppData();
-AppData.prototype.eventlisteners();
-
-// console.log(appData);
+appData.eventListeners();
 
 
 function checkSalaryAmount() {
   start.disabled = true;
-  if (salaryAmount.value.length != 0) {
+  if (salaryAmount.value.trim() !== '') {
+    salaryAmount.value = salaryAmount.value.trim();
     start.disabled = false;
   }
 }
